9ff6e0cd4042b4b14a1476ca1b46d6ac5c6ec4a0
Mehroz farooq <mehrozfarooq127@gmail.com>
2025-03-25 17:44:27 +0500

    first commit

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 
-import express from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -17,4 +17,6 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
+app.get("/", (req :Request, res:Response) => {res.send("Hello from server")});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
